407be48d1c41535c0b55fc2ba3428638d0a15044
KLam <lam.ksen@gmail.com>
2015-02-21 15:50:14 -0600

    Adding new template page

--- public/javascripts/views/search.js
+++ public/javascripts/views/search.js
@@ -16,22 +16,23 @@ define([
 
 		// Instead of generating a new element, bind to the existing skeleton of
 		// the App already present in the HTML.
-		el: '.search-container',
+		el: '.search-component',
 
 		// // Compile our stats template
 		// template: _.template(statsTemplate),
 
 		// Delegated events for creating new items, and clearing completed ones.
 		events: {
-			'keypress .search-box':		'createOnEnter',
+			'keyup .search-box':		'createOnEnter',
 		},
 
 		initialize: function () {
 			this.$input = this.$('.search-box');
 
-			this.$resultsList = this.$('.search-results');
+			this.$resultsList = this.$('.search-results-list');
 			
 			this.listenTo(SearchResultsCollection, 'add', this.addOne);
+			this.listenTo(SearchResultsCollection, 'reset', this.addAll);
 
 		},
 
@@ -47,10 +48,15 @@ define([
 			var view = new ResultItemView({ model: model });
 			this.$resultsList.append(view.render().el);
 		},
+		// Add all items in the **Todos** collection at once.
+		addAll: function () {
+			this.$resultsList.empty();
+			SearchResultsCollection.each(this.addOne, this);
+		},
 
 		createOnEnter: function (e) {
 			var searchTerm = this.$input.val().trim();
-
+			console.log('Searching for: ', searchTerm);
 			$.get('/api/search', { q : searchTerm }, $.proxy(this.processSearchResponse, this));
 
 			if (e.which !== Common.ENTER_KEY || !this.$input.val().trim()) {
@@ -65,6 +71,9 @@ define([
 			var self = this,
 				results = data.result;
 
+			// Clear previous results
+			SearchResultsCollection.reset();
+			
 			$.each(results, function () {
 				SearchResultsCollection.add(this);
 			});
@@ -72,21 +81,21 @@ define([
 	});
 
 
-	//ajax emulation. Type "err" to see error message
-	$.mockjax({
-	    url: '/api/search',
-	    responseTime: 400,
-	    response: function(settings) {
-	        if(settings.data.value == 'err') {
-	           this.status = 500;  
-	           this.responseText = 'Validation error!'; 
-	        } else {
-	           this.responseText = { 
-	           		"result":[{"name":"kasen Lam","expertise":"Node","weight":"100"},{"name":"kasen Lam","expertise":"HTML","weight":"200"},{"name":"Pablo Garbanzo","expertise":"Notepad","weight":"200"}]
-	           };  
-	        }
-	    }
-	}); 
+	// //ajax emulation. Type "err" to see error message
+	// $.mockjax({
+	//     url: '/api/search',
+	//     responseTime: 400,
+	//     response: function(settings) {
+	//         if(settings.data.value == 'err') {
+	//            this.status = 500;  
+	//            this.responseText = 'Validation error!'; 
+	//         } else {
+	//            this.responseText = { 
+	//            		"result":[{"name":"Liam Brown","expertise":"HTML5","expertiseList":["HTML5","Angular","Saas","Nodejs","Flash"],"weight":500,"idUser":1,"photo":"/images/people/perfil_1.png","phone":"+506-83097721","email":"liam.qa+testing@whoknows.com","occupation":"UX Designer"},{"name":"Patricia Morgan","expertise":"HTML5","expertiseList":["Angular","HTML5","Nodejs","Flash","Saas"],"weight":400,"idUser":1,"photo":"/images/people/perfil_3.png","phone":"+506-85239345","email":"patricia.qa+testing@whoknows.com","occupation":"Web Developer"},{"name":"Margareth Smith","expertise":"HTML5","expertiseList":["Flash","Saas","HTML5","Nodejs","Angular"],"weight":300,"idUser":1,"photo":"/images/people/perfil_5.png","phone":"+506-82324322","email":"margareth.qa+testing@whoknows.com","occupation":"Frontend Developer"},{"name":"David James","expertise":"HTML5","expertiseList":["Saas","Angular","Flash","HTML5","Nodejs"],"weight":200,"idUser":1,"photo":"/images/people/perfil_2.png","phone":"+506-82221338","email":"david.qa+testing@whoknows.com","occupation":"Computer Engineer"},{"name":"Barbara Stuart","expertise":"HTML5","expertiseList":["Nodejs","Flash","Angular","Saas","HTML5"],"weight":100,"idUser":1,"photo":"/images/people/perfil_4.png","phone":"+506-81302322","email":"barbara.qa+testing@whoknows.com","occupation":"Backend Developer"}]
+	//            };  
+	//         }
+	//     }
+	// }); 
 
 	return AppView;
 });
