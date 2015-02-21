c915b354804092b29d4978e9ad34b2ced7f5919a
fabian <fabian.hernandez@hulihealth.com>
2015-02-21 16:01:33 -0600

    Pablo se me comporta si no quiere que lo pase al salon de masajes a frotarlo

--- data/delta/1.js
+++ data/delta/1.js
@@ -1,53 +1,167 @@
 use wk;
 db.profile.insert({
-	identificator : "klam",
-	name : "Kasen Lam",
+	identificator : 1,
+	name : "Liam Brown",
 	picture : "/images/people/perfil_1.png",
 	expertise : [
 		{ 
-			name : "javascript",
-			score : 9999
+			name : "HTML5",
+			score : 500
 		},
 		{ 
-			name : "HTML",
-			score : 9999
+			name : "Nodejs",
+			score : 400
 		},
 		{ 
-			name : "CSS",
-			score : 9999
+			name : "Saas",
+			score : 300
+		},
+		{ 
+			name : "Angular",
+			score : 200
+		},
+		{ 
+			name : "Flash",
+			score : 100
 		}
 	],
-
-	profession : "front-end developer",
-
+	profession : "UX Designer",
 	country : "Costa Rica",
-
-	email : "someone@kmail.com",
-
-	phone : "8888-8888",
-
+	email : "liam.qa+testing@whoknows.com",
+	phone : "+506-83097721",
 	location : 'San Jose. Costa Rica, Av 24',
-
 	projects : [
 			{
-				projectId : "wk",
-				projectName : "Who knows",
+				projectId : 1,
+				projectName : "Space X",
 				tech : [ 
 					{
-						name : "Javascript"
+						name : "HTML5"
 					}, 
 					{
-						name : "CSS"
+						name : "Saas"
 					}
 				],
 				members : [
 					{
-						name : "klam"
+						name : "David James"
+					},
+					{
+						name : "Barbara Stuart"
+					},
+					{
+						name : "Patricia Morgan"
 					}
 				],
-				picture : '/images/projects/portafolio_1.png',
+				picture : '/images/projects/portafolio_1b.png',
 
 				stars : 176
+			},
+			{
+				projectId : 2,
+				projectName : "Falaniux",
+				tech : [ 
+					{
+						name : "Flash"
+					}, 
+					{
+						name : "Nodejs"
+					}, 
+					{
+						name : "Angular"
+					}
+				],
+				members : [
+					{
+						name : "Margareth Smith"
+					},
+					{
+						name : "David James"
+					},
+					{
+						name : "Patricia Morgan"
+					}
+				],
+				picture : '/images/projects/portafolio_2b.png',
+				stars : 276
+			},
+			{
+				projectId : 3,
+				projectName : "Expertise.Me",
+				tech : [ 
+					{
+						name : "HTML5"
+					}, 
+					{
+						name : "Nodejs"
+					}, 
+					{
+						name : "Saas"
+					}
+				],
+				members : [
+					{
+						name : "Margareth Smith"
+					},
+					{
+						name : "Patricia Morgan"
+					}
+				],
+				picture : '/images/projects/portafolio_3b.png',
+				stars : 301
+			},
+			{
+				projectId : 4,
+				projectName : "What the Zig!",
+				tech : [ 
+					{
+						name : "Nodejs"
+					}, 
+					{
+						name : "Angular"
+					}, 
+					{
+						name : "Flash"
+					}
+				],
+				members : [
+					{
+						name : "Patricia Morgan"
+					},
+					{
+						name : "David James"
+					},
+					{
+						name : "Barbara Stuart"
+					}
+				],
+				picture : '/images/projects/portafolio_4b.png',
+				stars : 434
+			},
+			{
+				projectId : 5,
+				projectName : "Essentials",
+				tech : [ 
+					{
+						name : "Angular"
+					}, 
+					{
+						name : "Nodejs"
+					}, 
+					{
+						name : "HTML5"
+					}
+				],
+				members : [
+					{
+						name : "Margareth Smith"
+					},
+					{
+						name : "David James"
+					}
+				],
+				picture : '/images/projects/portafolio_5b.png',
+				stars : 244
 			}
 		]
-})
+});
