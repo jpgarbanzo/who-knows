415d79050162ff99df21135545e7a21601463ca1
KLam <lam.ksen@gmail.com>
2015-02-21 11:21:16 -0600

    Adding base boilerplate

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { layout:false, title: '' });
+  res.render('index', { layout:'layouts/main', title: '' });
 });
 
 module.exports = router;
